9f90555c9b2478553336b7ff466caaa2b2ee8f0c
munigithari <naramalaharish2901@gmail.com>
2024-03-28 10:47:16 +0000

    error ocuured

---
+++ src/main/java/com/example/player/PlayerController.java
@@ -0,0 +1,49 @@
+/*
+ * You can use the following import statements
+ * 
+ * import org.springframework.web.bind.annotation.*;
+ * import java.util.ArrayList;
+ * 
+ */
+
+// Write your code here.
+package com.example.player;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import java.util.*;
+
+@RestController
+public class PlayerController {
+    PlayerService service = new PlayerService();
+
+    @GetMapping("/players")
+    public ArrayList<Player> getPlayers() {
+
+        return service.getPlayers();
+    }
+
+    @GetMapping("/players/{playerId}")
+    public Player getPlayerById(@PathVariable("playerId") int playerId) {
+
+        return service.getPlayerById(playerId);
+    }
+
+    @PostMapping("/players")
+    public Player addPlayer(@RequestBody Player player) {
+        return service.addPlayer(player);
+    }
+
+    @PutMapping("/players/{playerId}")
+    public Player updatePlayer(@PathVariable("playerId") int playerId, @RequestBody Player player) {
+
+        return service.updatePlayer(playerId, player);
+    }
+
+    @DeleteMapping("/players/{playerId}")
+    public void deletePlayer(@PathVariable("playerId") int playerId) {
+        service.deletePlayer(playerId);
+    }
+}
